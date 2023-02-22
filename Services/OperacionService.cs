b3519031bc51f86537f0326570a03b1f0ed81490
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-02-22 18:00:44 +0100

    cositas.

--- Services/OperacionService.cs
+++ Services/OperacionService.cs
@@ -7,16 +7,12 @@ public class OperacionService : OperacionServiceInterface
         _context = context;
     }
 
-    // static List<OperacionEntity> Operaciones { get; }
-    // static int nextId = 1;
-
     public List<OperacionEntity> GetAll() => _context.Operaciones.ToList();
 
     public OperacionEntity Get(int id) => _context.Operaciones.ToList().FirstOrDefault(p => p.idOperacion == id);
 
     public void Add(OperacionEntity operacion)
     {
-        // operacion.idOperacion = nextId++;
         _context.Add(operacion);
         _context.SaveChanges();
     }
@@ -36,18 +32,5 @@ public class OperacionService : OperacionServiceInterface
         _context.Update(operacionEntity);
         _context.SaveChanges();
     }
-    // public void mostrarOperacion()
-    // {
-    //     Console.WriteLine("[bold #13D7F6]Id de la operación: [/][bold white]" + _operacionEntity.idOperacion + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Nombre: [/][bold white]" + _operacionEntity.eventoComprado.nombre + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Cantante: [/][bold white]" + _operacionEntity.eventoComprado.cantante + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Localidad: [/][bold white]" + _operacionEntity.eventoComprado.localidad + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Categoría: [/][bold white]" + _operacionEntity.eventoComprado.categoria + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Fecha: [/][bold white]" + _operacionEntity.eventoComprado.fecha + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Entradas compradas: [/][bold white]" + _operacionEntity.numEntradasCompradas + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Precio total: [/][bold white]" + _operacionEntity.precioTotal + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Fecha de compra: [/][bold white]" + _operacionEntity.fechaCompra + "[/]");
-    //     Console.WriteLine(" ");
-    // }
 }
 
--- Services/UsuarioService.cs
+++ Services/UsuarioService.cs
@@ -7,16 +7,12 @@ public class UsuarioService : UsuarioServiceInterface
         _context = context;
     }
 
-    // static List<UsuarioEntity> usuarioEntityList { get; }
-    // static int nextId = 2;
-
     public List<UsuarioEntity> GetAll() => _context.Usuarios.ToList();
 
     public UsuarioEntity Get(int id) => _context.Usuarios.ToList().FirstOrDefault(p => p.idUsuario == id);
 
     public void Add(UsuarioEntity usuario)
     {
-        // usuario.idUsuario = nextId++;
         _context.Usuarios.Add(usuario);
         _context.SaveChanges();
     }
@@ -36,20 +32,5 @@ public class UsuarioService : UsuarioServiceInterface
         _context.Update(usuarioEntity);
         _context.SaveChanges();
     }
-
-    // public void listarInformacionUsuario()
-    // {
-    //     Console.WriteLine("[bold #13D7F6]Id: [/][bold white]" + _usuarioEntity.idUsuario + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Nombre: [/][bold white]" + _usuarioEntity.nombre + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Apellido: [/][bold white]" + _usuarioEntity.apellido + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Email: [/][bold white]" + _usuarioEntity.email + "[/]");
-    //     Console.WriteLine("[bold #13D7F6]Contraseña: [/][bold white]*********[/]");
-    // }
-
-    // public UsuarioEntity GetUsuarioEntityById(int idUsuario){
-    //     return ;
-    // }
-
-    // public List<>
 }
 
