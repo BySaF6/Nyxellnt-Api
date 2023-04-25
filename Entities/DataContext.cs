067b6e9a7789780b0bad5cbc1a3836565d1dcf8c
Samuel <samarandaf6@gmail.com>
2023-04-25 20:51:48 +0200

    Creada entity, controller y service de merchandising

--- Entities/DataContext.cs
+++ Entities/DataContext.cs
@@ -8,5 +8,6 @@ namespace NyxellntAPI.Entities
         public DbSet<EventoEntity> Eventos { get; set; }
         public DbSet<OperacionEntity> Operaciones { get; set; }
         public DbSet<UsuarioEntity> Usuarios { get; set; }
+        public DbSet<MerchandisingEntity> Merchandising { get; set; }
     }
 }
