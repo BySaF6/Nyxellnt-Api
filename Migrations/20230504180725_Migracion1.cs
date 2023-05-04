e1bebb8bb8edf94e59e37afef8c81ca5723f8254
Samuel <samarandaf6@gmail.com>
2023-05-04 20:15:37 +0200

    Nuevo campo de usuario y migraciones

---
+++ Migrations/20230504180725_Migracion1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NyxellntAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracion1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "rol",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "rol",
+                table: "Usuarios");
+        }
+    }
+}
